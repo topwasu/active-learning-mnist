7d318d441fb884931afe711ebc6f0a4e8cd45e77
topwasu <wasu.piri@gmail.com>
2019-12-12 17:38:30 -0500

    Add get_least_confident Need to be tested

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ def read_images(dir):
 	"""
 	read, normalize, and upsample images
 	:param dir: str, directory path
-	:return: 4-d tensor and list of str, the images and their names
+	:return: 4-d tensor and ndarray of str, the images and their names
 	"""
 	imagenet_mean = np.asarray([0.485, 0.456, 0.406])
 	imagenet_std = np.asarray([0.229, 0.224, 0.225])
@@ -43,7 +43,27 @@ def read_images(dir):
 			normalized_rgbimg = rgbimg - imagenet_mean / imagenet_std
 			list_imgs.append(torch.from_numpy(normalized_rgbimg))
 	list_imgs = torch.stack(list_imgs)
-	return torch.nn.functional.interpolate(list_imgs, (224, 224)), list_imgs_names
+	return torch.nn.functional.interpolate(list_imgs, (224, 224)), np.asarray(list_imgs_names)
+
+
+def get_least_confident(images, images_names, model, num_least_confident, batch_size=0):
+	model.eval()
+	num_len = images.shape[0]
+	ct = 0
+	list_logits = []
+	while ct < num_len:
+		batch_images = images[ct:min(ct + batch_size, num_len)]
+		
+		if torch.cuda.is_available():
+			batch_images = batch_images.cuda()
+		
+		logits = model(batch_images)
+		list_logits.append(logits)
+		
+		ct += batch_size
+	all_logits = torch.cat(list_logits).cpu().detach().numpy()
+	confidence = all_logits.max(axis=1)
+	return np.argsort(confidence)[:num_least_confident]
 
 
 def calc_accuracy(logits, labels):
@@ -113,6 +133,7 @@ def test(images, labels, model, batch_size=64):
 	:param batch_size: int, size of batch
 	:return: float, performance of the model on the test set
 	"""
+	model.eval()
 	num_len = labels.shape[0]
 	ct = 0
 	total_acc = 0
@@ -155,15 +176,24 @@ def main():
 	images_names = train_images_names[indices]
 	labels = request_labels(images_names, train_df)
 	
-	train(labeled_images, labels, resnet, 100, batch_size=64, verbose=True)
+	train(labeled_images, labels, resnet, 100, verbose=True)
 	
-	print(f'First checkpoint accuracy: {test(test_images, test_labels, resnet)}')
+	print(f'Checkpoint 1 accuracy: {test(test_images, test_labels, resnet)}')
 	# --------------------------------------------------------------------------
 	
-	# TODO: Second step
-	rest_train_images = train_images[np.logical_not(np.isin(train_images_names, images_names))]
-	
-	# TODO: Last step
+	# Second and Third Step-----------------------------------------------------
+	for cp in range(2, 4, 1):
+		rest_train_images = train_images[np.logical_not(np.isin(train_images_names, images_names))]
+		rest_train_images_names = train_images_names[np.logical_not(np.isin(train_images_names, images_names))]
+		wanted_indices = get_least_confident(rest_train_images, rest_train_images_names, resnet, 1000)
+		labeled_images = torch.cat([labeled_images, rest_train_images[wanted_indices]])
+		images_names = np.concatenate(images_names, rest_train_images_names[wanted_indices])
+		labels = request_labels(images_names, train_df)
+		
+		train(labeled_images, labels, resnet, 100, verbose=True)
+		
+		print(f'Checkpoint {cp} accuracy: {test(test_images, test_labels, resnet)}')
+	# --------------------------------------------------------------------------
 	
 
 if __name__ == '__main__':
